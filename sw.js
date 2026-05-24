e9529d1f4acf3f4e6de75a3054f6887af0b671b6
mmc-autoparts <mmcfirebase2026@gmail.com>
2026-05-25 00:54:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
-const CACHE_NAME = "mmc-portal-v54";
+const CACHE_NAME = "mmc-portal-v54"; // UPDATE THIS EVERY TIME YOU CHANGE THE APP
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(["./", "./index.html"]);
+        })
+    );
+});
+
+// 🚨 THIS IS THE MISSING PIECE! The Garbage Collector 🚨
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // If the old cache name doesn't match the new one, delete it!
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("Deleting old cache:", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
