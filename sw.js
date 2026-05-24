0a39b7edda3db71b030e0f538386ab49e31af781
mmc-autoparts <mmcfirebase2026@gmail.com>
2026-05-24 22:32:47 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "mmc-portal-v50";
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(["./", "./index.html"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
