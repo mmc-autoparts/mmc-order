db140ce41482fffc7840c429c6cfb80984a43059
mmc-autoparts <mmcfirebase2026@gmail.com>
2026-05-25 01:34:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mmc-portal-v54"; // UPDATE THIS EVERY TIME YOU CHANGE THE APP
+const CACHE_NAME = "mmc-portal-v55"; // UPDATE THIS EVERY TIME YOU CHANGE THE APP
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
